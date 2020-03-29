98aefdd19d9d36d4ccec217629221c9fb8f80b3c
sanchit <sanchit@cs.wisc.edu>
2020-03-28 21:45:45 -0400

    Changed spacing from tabs to spaces

--- central-server/tests/testClient.py
+++ central-server/tests/testClient.py
@@ -8,22 +8,19 @@ import centralserver_pb2_grpc
 
 
 def sendItems():
-	dictionary = {"foo": "bar", "loo": "poo"}
-	for k,v in dictionary.items():
-		yield centralserver_pb2.SetRequest(key=k, value=v)
+    dictionary = {"foo": "bar", "loo": "poo"}
+    for k,v in dictionary.items():
+        yield centralserver_pb2.SetRequest(key=k, value=v)
 
 
 def run():
-	with grpc.insecure_channel('localhost:50051') as channel:
-		stub = centralserver_pb2_grpc.CentralServerStub(channel)
-		stub.setValuesForKeys((sendItems()))
-		response = stub.getValue(centralserver_pb2.ValueRequest(key="foo"))
-		#response = stub.acceptCache((sendItems()))
-		#if response.success == True:
-			#print("Cache transferred")
-		print(response.value)
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = centralserver_pb2_grpc.CentralServerStub(channel)
+        stub.setValuesForKeys((sendItems()))
+        response = stub.getValue(centralserver_pb2.ValueRequest(key="foo"))
+        print(response.value)
 
 
 if __name__ == '__main__':
-	logging.basicConfig()
-	run()
+    logging.basicConfig()
+    run()
