e184079ce2dcd94b475cf2d472e6e9e2a94406f5
sanchit <sanchit@node-0.geronimo.uwmadison744-f18-pg0.clemson.cloudlab.us>
2020-04-17 12:12:01 -0400

    Experiment 2 part 2 - cache is not transferred

---
+++ edgeServer/nonCoordinatingEdgeServer.py
@@ -0,0 +1,169 @@
+from concurrent import futures
+import time
+import math
+import logging
+
+import grpc
+
+import kvstore_pb2
+import kvstore_pb2_grpc
+
+import centralserver_pb2
+import centralserver_pb2_grpc
+
+import yaml
+import sys
+from threading import Lock
+from threading import Timer
+
+import collections
+
+
+INVALID_SESSION = "invalid_session"
+INITIAL_CACHE_CAPACITY = 2000
+
+class User:
+	def __init__(self, sessID, clientID, currentEpoch):
+		self.sessionID = sessID
+		# not sure why we'd need clientID, but leaving it here for the future
+		self.clientID = clientID
+		self.cache = LRUCache(INITIAL_CACHE_CAPACITY)
+		self.epoch = currentEpoch
+
+
+class LRUCache:
+	# @param capacity, an integer
+	def __init__(self, capacity): #capacity- number of rows allowed
+		self.incrementSize = capacity
+		self.maxCacheCapacity = 5*capacity
+		self.capacity = capacity
+		self.currentCache = collections.OrderedDict()
+
+	# @return an integer
+	def get(self, key):
+		if key not in self.currentCache:
+			return None
+		else:       
+			value = self.currentCache.pop(key)
+		self.currentCache[key] = value
+		return value
+
+	# @param key, an integer
+	# @param value, an integer
+	# @return nothing
+	def set(self, key, value):
+		if key in self.currentCache:
+			self.currentCache.pop(key)
+		elif len(self.currentCache) == self.capacity:
+			if(self.capacity == self.maxCacheCapacity):
+				self.currentCache.popitem(last=False)
+			else:
+				self.capacity += self.incrementSize
+		self.currentCache[key] = value
+
+
+class KVStoreServicer(kvstore_pb2_grpc.MultipleValuesServicer):
+	"""Provides methods that implement functionality of Multiple Values Servicer."""
+
+	def __init__(self, serverID):
+		#self.cache = LRUCache(6) #key - client specific key, Value - List (value, timestamp)
+		self.serverID = serverID
+		self.activeSessionIDs = {}    
+		with open("neighbouringEdgeServer.yaml") as file:
+			self.neighboringEdgeServers = yaml.safe_load(file)
+		self.centralServerConn = self.connectCentralServer()  
+		self.epoch = -1
+		self.changeEpochAndCollectGarbage()
+
+	def changeEpochAndCollectGarbage(self):
+		toRemove= []
+		self.epoch += 1
+		for sessionID, user in self.activeSessionIDs.items():
+			# race conditions possible. They were also possible in the old design
+			if user.epoch <= self.epoch - 2:
+				toRemove.append(sessionID)
+		for entry in toRemove:
+			del self.activeSessionIDs[entry]
+		Timer(300, self.changeEpochAndCollectGarbage).start() #collecting garbage in background 20 mins
+	
+	def connectCentralServer(self):
+		channel = grpc.insecure_channel(self.neighboringEdgeServers["centralServer"])         
+		return centralserver_pb2_grpc.CentralServerStub(channel)
+	
+	def writeToCentralServer(self, key, value):
+		return self.centralServerConn.setValue(centralserver_pb2.CentralServerSetRequest(key=key, value = value))
+	
+	def readFromCentralServer(self, key):
+		return self.centralServerConn.getValue(centralserver_pb2.CentralServerValueRequest(key=key))
+
+
+	def bindToServer(self, request, context): #to establish the session for the first time        
+		# explicit binding is not always required if cache isn't transferred
+		sessionID = request.clientID + "-" + str(time.time())
+		self.activeSessionIDs[sessionID] = User(sessionID, request.clientID, self.epoch)
+		#self.activeSessionIDs.add(sessionID) #remove once session shift
+		return kvstore_pb2.sToken(clientID = request.clientID, serverID = self.serverID, sessionID = sessionID)
+	
+
+	def setValue(self, request, context):
+		if(request.token.sessionID not in self.activeSessionIDs.keys()):
+			# implicit binding
+			sessionID = request.token.clientID + "-" + str(time.time())
+			self.activeSessionIDs[sessionID] = User(sessionID, request.token.clientID, self.epoch)
+			request.token.serverID = self.serverID
+			request.token.sessionID = sessionID   
+
+		self.activeSessionIDs[request.token.sessionID].epoch = self.epoch
+
+		centralServerResponse = self.writeToCentralServer(request.key, request.value)
+		if(centralServerResponse.success):
+			currentTime = time.time()          
+			self.activeSessionIDs[request.token.sessionID].cache.set(request.key, [request.value, currentTime])
+			return kvstore_pb2.SetResponse(key=request.key, success=True, token = request.token)
+		else:
+			return kvstore_pb2.SetResponse(key=request.key, success=False, token = request.token)
+
+	def getValue(self, request, context):
+		if(request.token.sessionID not in self.activeSessionIDs.keys()):
+			#implicit binding
+			sessionID = request.token.clientID + "-" + str(time.time())
+			self.activeSessionIDs[sessionID] = User(sessionID, request.token.clientID, self.epoch)
+			request.token.serverID = self.serverID
+			request.token.sessionID = sessionID
+		
+		self.activeSessionIDs[request.token.sessionID].epoch = self.epoch #making the session a part of current epoch
+
+		toReturn = self.activeSessionIDs[request.token.sessionID].cache.get(request.key)
+		if(not toReturn):
+			#fetch from central server
+			centralServerResponse = self.readFromCentralServer(request.key)
+			currentTime = time.time()
+			if(centralServerResponse.value is not None):       
+				self.activeSessionIDs[request.token.sessionID].cache.set(request.key, [centralServerResponse.value, currentTime])
+				toReturn = [centralServerResponse.value, currentTime]
+			else:
+				return kvstore_pb2.ValueResponse(key=request.key, value = None, timeStamp = None, token = request.token)
+
+		return kvstore_pb2.ValueResponse(key=request.key, value = toReturn[0], timeStamp = toReturn[1], token = request.token)
+
+
+
+def serve(serverID):
+	with open("neighbouringEdgeServer.yaml") as file:
+		neighboringEdgeServers = yaml.safe_load(file)
+	server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+	kvstore_pb2_grpc.add_MultipleValuesServicer_to_server(
+		KVStoreServicer(serverID), server)    
+	connectionInfo = neighboringEdgeServers[serverID]
+	print('Starting server. Listening on port: ', connectionInfo)
+	server.add_insecure_port(connectionInfo)
+	server.start()
+	server.wait_for_termination()
+
+
+if __name__ == '__main__':
+	if(len(sys.argv) != 2):
+		print("Usage: python3 edgeServer.py <serverID as defined in yaml>")
+		exit(1)
+	logging.basicConfig()
+	serve(sys.argv[1])
---
+++ testScripts/exp2_part2.sh
@@ -0,0 +1,8 @@
+# Run after collecting the outputs of part 1, as they'll be over-written
+python3 spawnClientsForExp2Part2.py 1
+python3 spawnClientsForExp2Part2.py 2
+python3 spawnClientsForExp2Part2.py 4
+python3 spawnClientsForExp2Part2.py 8
+python3 spawnClientsForExp2Part2.py 16
+python3 spawnClientsForExp2Part2.py 32
+python3 spawnClientsForExp2Part2.py 64
